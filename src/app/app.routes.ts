d9b1ee87f97ed782c673634f1984ac92623b4aaa
Gyromitte <gyromitte@gmail.com>
2024-03-08 15:10:38 -0600

    Agregado ruta wild card y redireccion default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './pages/signup/signup.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 
-export const routes: Routes = [ 
+export const routes: Routes = [
     {path: 'registro', component: SignupComponent},
+    //TODO: cambiar la redireccion default a la ruta de ingresar
+    { path: '', redirectTo: '/registro', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent },
     ];
