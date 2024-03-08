51eea4322412f96b136f45a80e76d2e18b4b14db
Gyromitte <gyromitte@gmail.com>
2024-03-08 14:53:39 -0600

    Pagina WildCard 404
    Se creo una pagina para errores de redireccionamiento 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './pages/signup/signup.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RouterModule } from '@angular/router';
 
-export const routes: Routes = [ {path: 'registro', component: SignupComponent} ];
+export const routes: Routes = [ 
+    {path: 'registro', component: SignupComponent},
+    { path: '**', component: PageNotFoundComponent },
+    ];
