3858701a5896dbbd0d3fb17179c9c9a74d92fccf
Gyromitte <gyromitte@gmail.com>
2024-03-07 19:15:58 -0600

    Navbar y Botones
    Cree el componente de la navbar y los botones, los botones tienen como opcion agregar rutas

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../buttons/button/button.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
