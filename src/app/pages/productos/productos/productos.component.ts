8968df2b9ef809aedcdc67e870e490488de996e1
RicardoGGx <133252795+RicargoGGx@users.noreply.github.com>
2024-03-11 11:41:17 -0600

    productsComponent

--- src/app/pages/productos/productos/productos.component.ts
+++ src/app/pages/productos/productos/productos.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavbarDashComponent } from '../../../navbar-dash/navbar-dash.component';
 import { DynamicTableComponent } from '../../../dynamic-table/dynamic-table.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-productos',
@@ -10,5 +11,44 @@ import { DynamicTableComponent } from '../../../dynamic-table/dynamic-table.comp
   styleUrl: './productos.component.css'
 })
 export class ProductosComponent {
+  constructor(private http:HttpClient){}
+  productos: any[] = [];
 
+  obtenerProductos(){
+    const endpoint = "http://127.0.0.1:8000/api/products";
+    this.http.get<any[]>(endpoint).subscribe(
+      (data:any[]) => {
+        this.productos = data;
+      },
+      error => {
+        console.error('Error al obtener los productos', error);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.obtenerProductos();
+  }
+
+  botonesAccion = [
+    {nombre: 'Editar', accion: this.editarProducto},
+    {nombre: 'Eliminar', accion: this.eliminarProducto, clase:'btn-eliminar'}
+  ];
+
+  eliminarProducto(producto: any) {
+    const endpoint = `http:/}/127.0.0.1:8000/api/products/${producto.id}/deactivate`;
+    this.http.put(endpoint, {}).subscribe(
+      (response: any) => {
+        console.log('Producto desactivado:', producto);
+        this.obtenerProductos();
+      },
+      error => {
+        console.error('Error al desactivar producto:', error)
+      }
+    )
+  }
+  editarProducto(producto: any) {
+    console.log('Editar producto:', producto);
+    // Agrega aquí la lógica para editar el usuario
+  }
 }
