3d3b551365347f7517006184457e2449101520e0
Gyromitte <gyromitte@gmail.com>
2024-03-11 11:40:43 -0600

    Fix

--- src/app/pages/usuarios/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios/usuarios.component.ts
@@ -2,22 +2,24 @@ import { Component } from '@angular/core';
 import { NavbarDashComponent } from '../../../navbar-dash/navbar-dash.component';
 import { DynamicTableComponent } from '../../../dynamic-table/dynamic-table.component';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [NavbarDashComponent, DynamicTableComponent],
+  imports: [NavbarDashComponent, DynamicTableComponent, CommonModule],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css'
 })
 export class UsuariosComponent  {
   constructor(private http: HttpClient) {}
   usuarios: any[] = [];
+  notificacion: string = '';
 
   obtenerUsuarios() {
     const endpoint = 'http://127.0.0.1:8000/api/users'; // Reemplaza 'tu/endpoint/aqui' con tu URL real
-    this.http.get<any[]>(endpoint).subscribe(
-      (data: any[]) => {
+    this.http.get<Int16Array[]>(endpoint).subscribe(
+      (data: Int16Array[]) => {
         this.usuarios = data; // Asigna los datos de la respuesta de la API al arreglo de usuarios
       },
       error => {
@@ -32,20 +34,22 @@ export class UsuariosComponent  {
 
   botonesAccion = [
     { nombre: 'Editar', accion: this.editarUsuario },
-    { nombre: 'Eliminar', accion: this.eliminarUsuario, clase: 'btn-eliminar' }
+    { nombre: 'Eliminar', accion: (usuario: any) => this.eliminarUsuario(usuario), clase: 'btn-eliminar' }
     // Puedes agregar más botones de acción según tus necesidades
   ];
 
   eliminarUsuario(usuario: any) {
     const endpoint = `http://127.0.0.1:8000/api/users/${usuario.id}/deactivate`;
+  
     this.http.put(endpoint, {}).subscribe(
-      (response: any) => {
-        console.log('Usuario desactivado:', usuario);
-        // Actualiza la lista de usuarios después de desactivar uno
+      () => {
+        console.log('Usuario desactivado correctamente');
+        this.notificacion = 'Usuario desactivado correctamente';
         this.obtenerUsuarios();
       },
       error => {
         console.error('Error al desactivar usuario:', error);
+        this.notificacion = 'Error al desactivar usuario';
       }
     );
   }
