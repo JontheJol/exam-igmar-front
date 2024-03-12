a65ba0f598105eab7d40dca16b03ba764b090d45
Gyromitte <gyromitte@gmail.com>
2024-03-12 10:53:29 -0600

    Editar usuario
    Fix de rol y fix de ruta para editar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,15 +13,7 @@ import { ProductosComponent } from './pages/productos/productos/productos.compon
 
 import { CategoriasComponent } from './pages/categorias/categorias/categorias.component';
 import { EditCategoriaComponent } from './pages/categorias/edit-categoria/edit-categoria.component';
-import { AddCategoriaComponent } from './pages/categorias/add-categoria/add-categoria.component';
-
-import { ComentariosComponent } from './pages/commentarios/comentarios/comentarios.component';
-
-import { PlataformasComponent } from './pages/plataformas/plataformas/plataformas.component';
-import { EditPlataformaComponent } from './pages/plataformas/edit-plataforma/edit-plataforma.component';
-import { AddPlataformaComponent } from './pages/plataformas/add-plataforma/add-plataforma.component';
-
-
+import { VerificarCodigoComponent } from './pages/verificar-codigo/verificar-codigo.component';
 
 export const routes: Routes = [
     {path: 'registro', component: SignupComponent},
@@ -30,19 +22,11 @@ export const routes: Routes = [
     {path: 'dashboard/usuarios', component: UsuariosComponent},
     {path: 'dashboard/usuarios/edit/:id', component: EditUsuarioComponent },
     // Productos
-    {path: 'dashboard/usuarios/edit/:id', component: EditUsuarioComponent },
-    // Productos
     {path: 'dashboard/productos/productos', component: ProductosComponent},
     // Categorias
     {path: 'dashboard/categorias/categorias', component: CategoriasComponent},
     {path: 'dashboard/categorias/edit/:id', component: EditCategoriaComponent},
-    {path: 'dashboard/categorias/create', component: AddCategoriaComponent},
-    // Comentarios
-    {path: 'dashboard/comentarios/comentarios', component: ComentariosComponent},
-    // Plataformas
-    {path: 'dashboard/plataformas/plataformas', component: PlataformasComponent},
-    {path: 'dashboard/plataformas/edit/:id', component: EditPlataformaComponent},
-    {path: 'dashboard/plataformas/create', component: AddPlataformaComponent},
+
 
     {path: '', redirectTo: '/ingreso', pathMatch: 'full' },
     {path: '**', component: PageNotFoundComponent },
