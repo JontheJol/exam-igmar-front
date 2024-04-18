9ee42fad7d07d19883931b1a68017cbe33f9ea0d
JonJol <cjcb040404@hotmail.com>
2024-04-18 06:33:47 -0600

    Update view-score component to display results in a table and calculate the number of wins and losses

--- src/app/pages/view-score/view-score.component.ts
+++ src/app/pages/view-score/view-score.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TokenService } from '../../token.service';
 
 @Component({
   selector: 'app-view-score',
@@ -8,5 +9,43 @@ import { Component } from '@angular/core';
   styleUrl: './view-score.component.css'
 })
 export class ViewScoreComponent {
+
+  resultados: any[] = [];
+  gain:string = "Ganado";
+  who:string = "Quien";
+
+  ganados: number = 0;
+  perdidos: number = 0;
+
+  constructor(private dataService: TokenService) { }
+
+  ngOnInit() {
+    this.dataService.sendRequestWithToken("api/score",{}).subscribe((data: any) => {
+      for (const resultado of data) {
+        this.resultados.push({
+          resultado: resultado.resultado,
+          contra: resultado.contra
+        });
+        console.log(resultado);
+
+      }
+      this.gain = data.resultado;
+      this.who = data.contra;
+      this.calcularResultados();
+    });
+  }
+
+  calcularResultados() {
+    this.ganados = 0;
+    this.perdidos = 0;
+
+    for (const resultado of this.resultados) {
+      if (resultado.resultado === 'Ganado') {
+        this.ganados++;
+      } else {
+        this.perdidos++;
+      }
+    }
+  }
 
 }
