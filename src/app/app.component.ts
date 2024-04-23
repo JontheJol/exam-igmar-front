4306c285961e0397207b7709b0f4e1a294e6f2bf
Jorge706 <gogocumple12@gmail.com>
2024-04-23 12:14:28 -0600

    nueva rama

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
 import { ButtonComponent } from './buttons/button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent, ButtonComponent, RouterModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ButtonComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,38 +1,27 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent} from './pages/login/login.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { UsuariosComponent } from './pages/usuarios/usuarios/usuarios.component';
-import { EditUsuarioComponent } from './pages/usuarios/edit-usuario/edit-usuario.component';
-
-import { VerificarCodigoComponent } from './pages/verificar-codigo/verificar-codigo.component';
 import { ViewScoreComponent } from './view-score/view-score.component';
-import { LogListComponent } from './log-list/log-list.component';
 import { authGuard } from './guards/auth.guard';
 
 import { GameComponent } from './pages/game/game.component';
 import { LandingComponent } from './pages/landing/landing.component';
 // import { PruebaComponent } from './prueba/prueba.component';
+
 export const routes: Routes = [
     {path: 'registro', component: SignupComponent},
     {path: 'ingreso', component: LoginComponent},
 
-    {path: 'dashboard', component: DashboardComponent,canActivate:[authGuard]}, // TODO: no se que otra validacion necesitemos para acceder al dashboard
-    {path: 'dashboard/usuarios', component: UsuariosComponent},
-    {path: 'dashboard/usuarios/edit/:id', component: EditUsuarioComponent },
+    // {path: 'dashboard', component: DashboardComponent,canActivate:[authGuard]}, // TODO: no se que otra validacion necesitemos para acceder al dashboard
+    // {path: 'dashboard/usuarios', component: UsuariosComponent},
+    // {path: 'dashboard/usuarios/edit/:id', component: EditUsuarioComponent },
 
-    {path: 'landing', component: LandingComponent},
-    {path: 'game',component: GameComponent},
-    {path: 'view-score', component: ViewScoreComponent},
+    {path: 'landing', component: LandingComponent,canActivate:[authGuard]},
+    {path: 'game',component: GameComponent,canActivate:[authGuard]},
+    {path: 'view-score', component: ViewScoreComponent,canActivate:[authGuard]},
 
-    //WebSocket
-    // {path: 'prueba', component: PruebaComponent},
-//logs
-    {path: 'dashboard/logs', component: LogListComponent},
 
-     {path: '', redirectTo: '/ingreso', pathMatch: 'full' },
-    {path: '**', component: PageNotFoundComponent },
+    //  {path: '', redirectTo: '/landing', pathMatch: 'full' },
+    {path: '', component: SignupComponent },
+    {path: '**', component: SignupComponent },
     ];
